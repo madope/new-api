df99ab4422f3dc6277c267c39a1b09c5076eccf8
zhaoziyi <zhaoziyi@isurecloud.com>
2026-05-23 09:05:31 +0800

    fix: add tencent vod video to classic channel list

--- web/classic/src/constants/channel.constants.js
+++ web/classic/src/constants/channel.constants.js
@@ -189,6 +189,11 @@ export const CHANNEL_OPTIONS = [
     color: 'blue',
     label: 'Codex (OpenAI OAuth)',
   },
+  {
+    value: 58,
+    color: 'teal',
+    label: 'TencentVODVideo',
+  },
 ];
 
 // Channel types that support upstream model list fetching in UI.
--- web/classic/src/helpers/render.jsx
+++ web/classic/src/helpers/render.jsx
@@ -378,6 +378,7 @@ export function getChannelIcon(channelType) {
     case 19: // 360 智脑
       return <Ai360.Color size={iconSize} />;
     case 23: // 腾讯混元
+    case 58: // 腾讯云 VOD 视频
       return <Hunyuan.Color size={iconSize} />;
     case 31: // 零一万物
       return <Yi.Color size={iconSize} />;
